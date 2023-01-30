b979a333ff41b0c03a2ecfce6da78ab2c6f2807d
fmegahed <fmegahed@miamioh.edu>
2023-01-30 12:07:22 -0500

    isa444 spring23 class03

--- code_to_convert_xarigan_to_pdf_and_powerpoint.R
+++ code_to_convert_xarigan_to_pdf_and_powerpoint.R
@@ -1,15 +1,15 @@
 if(require(renderthis)==FALSE) remotes::install_github("jhelvy/renderthis", dependencies = TRUE)
 if(require(officer) == FALSE) install.packages('officer')
 
-renderthis::to_pdf(from = 'lectures/02_introduction_to_r/02_intro_r.html',
-                   to = 'pdfs/02_intro_r.pdf',
+renderthis::to_pdf(from = 'lectures/03_ts_viz/03_ts_viz.html',
+                   to = 'pdfs/03_ts_viz.pdf',
                    complex_slides = TRUE,
                    partial_slides = TRUE,
                    delay = 2)
 
 # I am using my adobe to create the ppt since the text is editable (which is more preferable to students)
-renderthis::to_pptx(from = 'lectures/02_introduction_to_r/02_intro_r.html',
-                    to = 'ppts/02_intro_r.pptx',
+renderthis::to_pptx(from = 'lectures/03_ts_viz/03_ts_viz.html',
+                    to = 'ppts/03_ts_viz.pptx',
                     complex_slides = TRUE,
                     partial_slides = TRUE,
                     delay = 2)

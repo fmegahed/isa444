bd0266a66380c11cba10701be045a2cff22ffb31
fmegahed <fmegahed@miamioh.edu>
2023-04-03 12:34:29 -0400

    spring 2023 lecture 19

--- code_to_convert_xarigan_to_pdf_and_powerpoint.R
+++ code_to_convert_xarigan_to_pdf_and_powerpoint.R
@@ -4,15 +4,15 @@
 if(require(renderthis)==FALSE) remotes::install_github("jhelvy/renderthis", dependencies = TRUE)
 if(require(officer) == FALSE) install.packages('officer')
 
-renderthis::to_pdf(from = 'lectures/18_arma/18_arma.html',
-                   to = 'pdfs/18_arma.pdf',
+renderthis::to_pdf(from = 'lectures/19_arima/19_arima.html',
+                   to = 'pdfs/19_arima.pdf',
                    complex_slides = TRUE,
                    partial_slides = TRUE,
                    delay = 2)
 
 # I am using my adobe to create the ppt since the text is editable (which is more preferable to students)
-renderthis::to_pptx(from = 'lectures/18_arma/18_arma.html',
-                    to = 'ppts/18_arma.pptx',
+renderthis::to_pptx(from = 'lectures/19_arima/19_arima.html',
+                    to = 'ppts/19_arima.pptx',
                     complex_slides = TRUE,
                     partial_slides = TRUE,
                     delay = 2)
